f86b564226c46466d580cb881a124ed4f2b5d69c
Joe <778337607@qq.com>
2026-02-17 21:13:48 +1100

    no message

---
+++ OrdersDemo/OrdersDemo/OrdersDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrdersDemoApp.swift
+//  OrdersDemo
+//
+//  Created by huanjiao qiu on 17/2/2026.
+//
+
+import SwiftUI
+
+@main
+struct OrdersDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
